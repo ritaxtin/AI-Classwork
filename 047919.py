a784de59cb0fb2bb24312067c9eafa7a3840305f
ritaxtin <ritaxtin07@gmail.com>
2015-09-30 11:22:27 +0300

    Update 047919.py

--- 047919.py
+++ 047919.py
@@ -1,12 +1,12 @@
 >>> import math
->>> list= [23,34,56,78,123,45,67,89,12,56]
->>> mean=(sum(list)/len(list))
+>>> list= [23,34,56,78,123,45,67,89,12,56] # generates list
+>>> mean=(sum(list)/len(list)) #formulae for computing the mean
 
->>> def findvar(list, mean):
-	var=0
+>>> def findvar(list, mean): # calculating the variance
+	var=0 # initialize var
 	for value in list:
-		var+=(value-mean)**2
-	variance=(sum(var))/len(list)
+		var+=(value-mean)**2 # computer the difference of the value on list from mean and squares the answer
+	variance=(var)/len(list) 
 	print variance
 	finddev(variance)
 
